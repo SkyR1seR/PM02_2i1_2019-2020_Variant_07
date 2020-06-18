c95a0c8f9bfa2372e9d0ca585aaa43b388926979
SkyR1seR <56387043+SkyR1seR@users.noreply.github.com>
2020-06-18 11:14:58 +0400

    МДК_02.02_пункт_2
    Всё что было до этого не в счёт

---
+++ Выполненное задание/МДК_02.02_мой_проект/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите X");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y = " + result(x));
+            Console.ReadKey();
+        }
+
+        public static int result(int x)
+        {
+            return (x + 2 * x * x + 3 * x);
+        }
+    }
+}
