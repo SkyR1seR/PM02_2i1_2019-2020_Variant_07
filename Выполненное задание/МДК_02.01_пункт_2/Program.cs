167fdf6b775aa001708221f78de81e11f2b92aa8
SkyR1seR <56387043+SkyR1seR@users.noreply.github.com>
2020-06-18 10:48:30 +0400

    МДК_02.02_пункт_2

---
+++ Выполненное задание/МДК_02.01_пункт_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите X");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y = " + (x+ 2*x*x+3*x));
+            Console.ReadKey();
+        }
+    }
+}
